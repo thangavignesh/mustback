109a3a913c1da1ffc47f6f44bba38ca86d1160c5
thangavignesh <thangavignesh@live.com>
2013-11-06 00:03:25 +0530

    Spot price woking for observer catalog save before

--- app/code/local/Mustback/Spotprice/Model/Observer.php
+++ app/code/local/Mustback/Spotprice/Model/Observer.php
@@ -11,47 +11,53 @@
 		 * @param   Varien_Event_Observer $observer
 		 * @return  Mustback_Spotprice_Model_Observer
 		 */
-		public function updatePrice($observer)
+		public function updateOIPriceCatBfSave($observer)
 		{
 			error_reporting(E_ALL);
 			ini_set( 'display_errors','1');
 			$event = $observer->getEvent();
 			$product = $event->getProduct();
 			
-			$metalType = $product->getMetalType();
-			$metalTypeName = $product->getAttributeText("metal_type");
-			$weight = $product->getWeight();
-			$feed = Mage::getModel('spotprice/feed')->getCollection()
-					 ->addFieldToFilter('metal_name', $metalTypeName)
-					 ->setOrder('create_time', 'desc')
-			         ->setCurPage(1)
-					 ->setPageSize(1);
-			$feedData = $feed->getData();
-			#Get metal recent price from feed_price table.
-			$metalPrice = $feedData[0]["value"];        
-			$manufacturingCost = $product->getManufacturingCost();
-			$newCost = ( $weight * $metalPrice ) + $manufacturingCost;
-			
-			#Save the new calculated cost.
-			$product->setPriceView($newCost)
-						  ->save();
+			#call the spot price business function
+			$product = $this->__applySpotPrice($product);
 						
-			print "<pre>";
-			print "Metal type: $metalType <br>";
-			print "Weight: $weight <br>";
-			print "Metal Type: $metalTypeName <br>";
-			print "Metal price: $metalPrice<br>";
-			print "Manufacturing Cost: $manufacturingCost<br>";
-			print "New Cost: $newCost<br>";
-			print_r($feed->getData());
-			print "End";	
-			print "</pre>";
-			#exit(0);
-			
-			
-			
-			$product->setPrice(($weight * 10));
 			return $this;
 		}
+
+		
+		/* Business Functions Begin*/
+		
+			private function __applySpotPrice($item)
+			{
+				#Get attributes of the Item
+				$itemPrice = $item->getPrice();
+				$itemMetalName = $item->getAttributeText("metal_type"); //custom attribute metal_type
+				$itemWeight = $item->getWeight();
+				$itemManufacturingCost = $item->getManufacturingCost();
+				
+				#Load market feed value
+				$feed = Mage::getModel('spotprice/feed')->getCollection()
+				->addFieldToFilter('metal_name', $itemMetalName)
+				->setOrder('create_time', 'desc')
+				->setCurPage(1)
+				->setPageSize(1);
+				
+				$feedData = $feed->getData();
+				#Get metal recent price from feed_price table.
+				$metalSpotPriceOunce = $feedData[0]["value"]; //Attribute value holding the price number
+				
+				#Convert units from ounce to gram
+				$metalSpotPriceGram = $metalSpotPriceOunce / 31.1034768; //1 troy ounce = 31.1034768 grams
+				
+				#Spot price formula
+				$calculatedSpotPrice = ( $itemWeight * $metalSpotPriceGram ) + $itemManufacturingCost;
+				
+				#Save the calculated price
+				$item->setPrice($calculatedSpotPrice);
+				
+				return $item;
+				
+			}
+		/* Business Functions End*/
 	}
 ?>
